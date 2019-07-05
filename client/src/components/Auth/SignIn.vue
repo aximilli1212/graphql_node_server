7d6853f54b42543f8c7e9ecf046c26612cda0f06
nii_sakie <aximilli1212@gmail.com>
2019-07-05 10:07:04 +0000

    Added SignIn Schema and GraphQL Variables

--- client/src/components/Auth/SignIn.vue
+++ client/src/components/Auth/SignIn.vue
@@ -1,11 +1,43 @@
 <template>
   <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1>Welcome Back!</h1>
+      </v-flex>
+    </v-layout>
 
+    <!-- Signin Form -->
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card color="secondary" dark>
+          <v-container>
+            <v-form @submit.prevent="handleSigninUser">
 
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field v-model="username" prepend-icon="face" label="Username" type="text" required></v-text-field>
+                </v-flex>
+              </v-layout>
+
+              <v-layout row>
+                <v-flex xs12>
+                  <v-text-field v-model="password" prepend-icon="extension" label="Password" type="password" required></v-text-field>
+                </v-flex>
+              </v-layout>
+
+              <v-layout row>
+                <v-flex xs12>
+                  <v-btn color="accent" type="submit">Signin</v-btn>
+                  <h3>Don't have an account?
+                    <router-link to="/signup">Signup</router-link>
+                  </h3>
+                </v-flex>
+              </v-layout>
+
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -14,8 +46,15 @@
   export default {
     name:"signin",
     data: () => ({
+      username:'',
+      password:''
 
-    })
+    }),
+    methods: {
+      handleSigninUser(){
+        
+      }
+    }
   }
 </script>
 
--- client/src/queries.js
+++ client/src/queries.js
@@ -21,3 +21,19 @@ export const GET_POSTS = gql`
 
 // Post Mutations
 // User Mutations
+
+export const SIGNIN_USER = gql`
+     mutation($username: String!, $password: String!){
+         signinUser(username:$username, password:$password){
+             token
+         }
+     }
+`;
+
+export const SIGNUP_USER = gql`
+    mutation($username:String!, $password:String!,$email:String!) {
+        signupUser(username:$username,password:$password,email:$email){
+            token
+        }
+    }
+`
